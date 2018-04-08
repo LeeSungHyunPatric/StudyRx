d807fd7a6239706a35aa50672313ceb608f86382
LeeSungHyun <jooosan011@gmail.com>
2018-04-08 19:56:01 +0900

    First Commit

--- Code/RxPractice/src/main/java/FirstPractice.java
+++ Code/RxPractice/src/main/java/FirstPractice.java
@@ -1,5 +1,14 @@
-import io.reactivex.Obsevable;
+import io.reactivex.*;
 
 public class FirstPractice {
 
+	public void enit() {
+		Observable.just("Hello Rxjava")
+		.subscribe(System.out::println);
+	}
+	
+	public static void main (String[] args) {
+		FirstPractice firstPractice = new FirstPractice();
+		firstPractice.enit();
+	}
 }
