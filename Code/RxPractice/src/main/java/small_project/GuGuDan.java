1d47430f3cc1c71c179bd70f63996586c0ee2c67
LeeSungHyun <jooosan011@gmail.com>
2018-04-23 22:30:35 +0900

    2018-4-23

--- Code/RxPractice/src/main/java/small_project/GuGuDan.java
+++ Code/RxPractice/src/main/java/small_project/GuGuDan.java
@@ -1,5 +1,24 @@
 package small_project;
 
-public class GuGuDan {
+import java.util.Scanner;
 
+import io.reactivex.Observable;
+import io.reactivex.functions.Function;
+
+public class GuGuDan {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		
+		Function<Integer, Observable<Integer>> fc = num -> {
+				Observable<Integer> ob = Observable.range(1, 9)
+						.map(it -> it*num);
+				return ob;
+		};
+		
+			Observable.range(1, 9)
+			.flatMap(fc)
+			.subscribe(System.out::println);
+	}
 }
